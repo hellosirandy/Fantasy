5a4af582ea73e1a18d181cca7fceb8a50a5236b3
hellosirandy <sac840711@gmail.com>
2016-10-13 17:03:48 +0800

    finish recommand

--- DraftRecommander.py
+++ DraftRecommander.py
@@ -1,5 +1,6 @@
 import requests
 import csv
+import operator
 
 
 class DraftRecommander:
@@ -8,8 +9,9 @@ class DraftRecommander:
         self.remainPool = {}
         self.othersDraftedPool = []
         self.myDraftedPool = []
-        self.othersAverageStats = {'FG%': 0, 'FT%': 0, '3PTM': 0, 'PTS': 0, 'REB': 0, 'AST': 0, 'ST': 0, 'BLK': 0, 'TO': 0}
-        self.myAverageStats = {'FG%': 0, 'FT%': 0, '3PTM': 0, 'PTS': 0, 'REB': 0, 'AST': 0, 'ST': 0, 'BLK': 0, 'TO': 0}
+        self.othersAverageStats = {'FGM': 0, 'FGA': 0, 'FG%': 0, 'FTM': 0, 'FTA': 0, 'FT%': 0, '3PTM': 0, 'PTS': 0, 'REB': 0, 'AST': 0, 'ST': 0, 'BLK': 0, 'TO': 0}
+        self.myAverageStats = {'FGM': 0, 'FGA': 0, 'FG%': 0, 'FTM': 0, 'FTA': 0, 'FT%': 0, '3PTM': 0, 'PTS': 0, 'REB': 0, 'AST': 0, 'ST': 0, 'BLK': 0, 'TO': 0}
+    
     def GetData(self):
         url = "http://stats.nba.com/leaders"
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.99 Safari/537.36'}
@@ -31,23 +33,23 @@ class DraftRecommander:
             results = response.json()
             self.rawData['headers'] = results['resultSet']['headers']
             self.rawData['rows'] = results['resultSet']['rowSet']
+
     def GetPlayers(self):
         for row in self.rawData['rows']:
             row = dict(zip(self.rawData['headers'], row))
-            self.remainPool[str(row['PLAYER'])] = {'FG%': row['FG_PCT'], 'FT%': row['FT_PCT'], '3PTM': row['FG3M']
-                , 'PTS': row['PTS'], 'REB': row['REB'], 'AST': row['AST'], 'ST': row['STL'], 'BLK': row['BLK']
-                , 'TO': row['TOV']}
+            self.remainPool[str(row['PLAYER'])] = {'FGM': row['FGM'], 'FGA': row['FGA'], 'FG%': row['FG_PCT'], 'FTM': row['FTM'], 'FTA': row['FTA'], 'FT%': row['FT_PCT'], '3PTM': row['FG3M'], 'PTS': row['PTS'], 'REB': row['REB'], 'AST': row['AST'], 'ST': row['STL'], 'BLK': row['BLK'], 'TO': row['TOV']}
+
     def OutputCSV(self):
         with open('stats.csv', 'w') as csvfile:
             fieldnames = ['Player', 'FG%', 'FT%', '3PTM', 'PTS', 'REB', 'AST', 'ST', 'BLK', 'TO']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
             writer.writeheader()
             for row in self.rawData['rows']:
                 row = dict(zip(self.rawData['headers'], row))
                 writer.writerow({'Player': row['PLAYER'], 'FG%': row['FG_PCT'], 'FT%': row['FT_PCT'], '3PTM': row['FG3M']
                     , 'PTS': row['PTS'], 'REB': row['REB'], 'AST': row['AST'], 'ST': row['STL'], 'BLK': row['BLK']
                     , 'TO': row['TOV']})
+
     def PutDraftedPool(self, playerName, identity):
         if identity == 'others':
             self.othersDraftedPool.append(playerName)
@@ -55,19 +57,32 @@ class DraftRecommander:
             self.myDraftedPool.append(playerName)
         self.UpdateAverage(playerName, identity)
         self.remainPool.pop(playerName, None)
+
     def UpdateAverage(self, playerName, identity):
         if identity == 'others':
             draftedNum = len(self.othersDraftedPool)
             for key, value in self.othersAverageStats.iteritems():
-                self.othersAverageStats[key] = (value * (draftedNum - 1) + self.remainPool[playerName][key]) / draftedNum
+                if key == 'FGM' or key == 'FGA' or key == 'FTM' or key == 'FTA':
+                    self.othersAverageStats[key] = value + self.remainPool[playerName][key]
+                elif not key == 'FG%' and not key == 'FT%':
+                    self.othersAverageStats[key] = (value * (draftedNum - 1) + self.remainPool[playerName][key]) / draftedNum
+            self.othersAverageStats['FG%'] = self.othersAverageStats['FGM'] / self.othersAverageStats['FGA']
+            self.othersAverageStats['FT%'] = self.othersAverageStats['FTM'] / self.othersAverageStats['FTA']
             print 'others average: ' + str(self.othersAverageStats)
         elif identity == 'me':
             draftedNum = len(self.myDraftedPool)
             print draftedNum
             for key, value in self.myAverageStats.iteritems():
-                self.myAverageStats[key] = (value * (draftedNum - 1) + self.remainPool[playerName][key]) / draftedNum
+                if key == 'FGM' or key == 'FGA' or key == 'FTM' or key == 'FTA':
+                    self.myAverageStats[key] = value + self.remainPool[playerName][key]
+                elif not key == 'FG%' and not key == 'FT%':
+                    self.myAverageStats[key] = (value * (draftedNum - 1) + self.remainPool[playerName][key]) / draftedNum
+            self.myAverageStats['FG%'] = self.myAverageStats['FGM'] / self.myAverageStats['FGA']
+            self.myAverageStats['FT%'] = self.myAverageStats['FTM'] / self.myAverageStats['FTA']
             print 'my average: ' + str(self.myAverageStats)
+
     def Recommand(self):
+        rank = {}
         maxInfluence = -100
         losingItems = {}
         recommandPlayer = ''
@@ -82,9 +97,17 @@ class DraftRecommander:
                 myValue = self.myAverageStats[lkey]
                 othersValue = self.othersAverageStats[lkey]
                 newPlayerValue = rvalue[lkey]
-                influence += (newPlayerValue - myValue) / (othersValue - myValue)
+                singleInfluence = (newPlayerValue - myValue) / (othersValue - myValue)
+                if singleInfluence > 1:
+                    singleInfluence = 1
+                influence += singleInfluence
+            rank[rkey] = influence
             if influence > maxInfluence:
                 maxInfluence = influence
                 recommandPlayer = rkey
-        print recommandPlayer
+        sortedRank = sorted(rank.items(), key = operator.itemgetter(1), reverse = True)
+
+        for i in range(0, 5):
+            print sortedRank[i][0]
+        #print recommandPlayer
 
--- MainApp.py
+++ MainApp.py
@@ -1,21 +1,26 @@
-from fantasy import DraftRecommander
+from DraftRecommander import DraftRecommander
 import sys
 
 DR = DraftRecommander()
 DR.GetData()
 DR.GetPlayers()
 while True:
-    sys.stdout.write('identity: >> ')
+    sys.stdout.write('Identity: (o or m) >> ')
     identity = raw_input()
     if  identity == 'o':         #others
-        sys.stdout.write('player name: >> ')
-        newDrafted = raw_input()
-        DR.PutDraftedPool(newDrafted, 'others')
+        try:
+            sys.stdout.write('Player name: >> ')
+            newDrafted = raw_input()
+            DR.PutDraftedPool(newDrafted, 'others')
+        except:
+            print 'No such player'
     elif identity == 'm':       #me
         DR.Recommand()
         try:
-            sys.stdout.write('player name: >> ')
+            sys.stdout.write('Player name: >> ')
             newDrafted = raw_input()
             DR.PutDraftedPool(newDrafted, 'me')
         except:
             print 'No such player'
+    else:
+        print "Please insert 'o' or 'm'"
